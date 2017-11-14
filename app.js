67a2d3ef896b798d34b0cadb4610daf38770af01
JTranie <julien.tranie@sciencespo.fr>
2017-11-14 16:03:00 +0100

    Quiz Temine

--- app.js
+++ app.js
@@ -17,18 +17,28 @@ var app = new Vue({
       // Modifie la propriété succès en fonction de la réponse (QCM)
       var correction = this.step.choices[this.step.answer];
 
-      // ******* Ecris ici la condition nécessaire
+      if (answer === correction) {
+        this.success = true;
+         } else { this.success = false;
+         }// ******* Ecris ici la condition nécessaire
     },
     submitText: function submitText () {
       // Modifie la propriété succès en fonction de la réponse (réponse libre)
       var answer = this.$refs.textInput.value;
       var correction = this.step.answer;
 
-      // ******* Ecris ici la condition nécessaire
+      if (answer === correction) {
+      this.success = true;
+         } else { this.success = false;
+         }// ******* Ecris ici la condition nécessaire
     },
     nextQuestion: function nextQuestion () {
       // Incrémente le score, réinitialise la propriété succès, passe à la question suivante
-
+      if (this.success === true) {
+        this.score++;
+      }
+      this.success = null;
+      this.current++;
       // ******* Ecris ici les actions et la condition nécessaires
     },
     buttonClasses: function buttonClasses (value) {
